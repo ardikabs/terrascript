7986835f3b1b2d37d571b36a5d1655acaced94f4
Ardika Bagus <me@ardikabs.com>
2020-09-23 13:13:03 +0700

    Add githooks post-commit

--- first_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# make sure the .githooks folder and its content is executable
-sudo chmod -R +x .githooks
-
-# set custom git hooks folder to .githooks
-git config core.hooksPath .githooks

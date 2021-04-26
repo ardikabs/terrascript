dd01c48608bc12136a55d64278130b00eb6ab947
Ardika Bagus S <me@ardikabs.com>
2021-04-26 16:05:50 +0700

    feat(terraformless): add fetch terraform capability (#10)
    since current build support terraformless,
    we need to add capability for terrascript itself to support
    instead of only from docker image

--- lib/util.sh
+++ lib/util.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+fetch_terraform_if_not_exist() {
+  if ! type terraform >/dev/null 2>&1 || [[ -x /usr/local/bin/terraform ]]; then
+    wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/"${TERRAFORM_VERSION}"/terraform_"${TERRAFORM_VERSION}"_linux_amd64.zip
+    unzip -qq -o /tmp/terraform.zip -d /tmp/; rm -rf /tmp/terraform.zip
+    mv "/tmp/terraform" "/tmp/terraform_${TERRAFORM_VERSION}"
+
+    chmod +x "/tmp/terraform_${TERRAFORM_VERSION}"
+
+    export TF_BIN="/tmp/terraform_${TERRAFORM_VERSION}"
+  fi
+}
+
 assigner_check() {
   assigners=$(echo "${TF_ASSIGNERS-}" | base64 -d)
 
